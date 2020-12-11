4a0d7b103223fd68765568f93fc2c4c26b7a78da
GavinZhang <zhanggan@cn.ibm.com>
2020-12-11 16:06:59 +0800

    updates

---
+++ src_java/jnipase/others/Hello.java
@@ -0,0 +1,7 @@
+/* File Hello.java created by Zhang Gan,917202 on Mon Aug  2 2010. */
+
+public class Hello {
+    public static void main(String args[]) {
+	System.out.println("HelloWorld");
+    }
+}
---
+++ src_java/rmi/HelloInterface.java
@@ -0,0 +1,7 @@
+import java.rmi.*;
+
+public interface HelloInterface extends Remote {
+
+	public String say() throws RemoteException;
+
+}
