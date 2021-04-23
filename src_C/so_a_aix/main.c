30904a6213b6f1c03568933e727f10979632c2c3
GavinZhang <zhanggan@cn.ibm.com>
2021-04-23 15:45:53 +0800

    add so_a_aix

--- src_C/so_a_aix/main.c
+++ src_C/so_a_aix/main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-void func1();
-void func2();
+extern void func1();
+extern void func2();
 int main(int argc, char * argv[]) {
-    printf("This in main.");
+    printf("This in main.\n");
     func1();
     func2();
     return(0);
---
+++ src_C/so_a_aix/mymain.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+void my_f1(int);
+void main(){
+	int s = 8;
+	my_f1(s);
+}
+
